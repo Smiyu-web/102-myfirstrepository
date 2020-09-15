82d0429e988b943b99ef7731a3888cbc0790c877
Miu <miu@MacBook-Pro.hitronhub.home>
2020-09-15 10:01:53 -0700

    added conditional examples

---
+++ conditionalsExample1.js
@@ -0,0 +1,11 @@
+let foodOne = "apple";
+let foodTwo = "orange";
+const favouriteFood = "apple";
+
+if (foodOne === favouriteFood) {
+    //do something here
+    console.log(foodOne);
+} else {
+    //do something here
+    console.log(foodTwo);
+};
---
+++ conditionalsExample2.js
@@ -0,0 +1,13 @@
+let foodOne = "apple";
+let foodTwo = "orange";
+const favouriteFood = "apple";
+
+if (foodOne === favouriteFood) {
+    //do something here
+    console.log(foodOne);
+} else if (foodTwo === favouriteFood) {
+    //do something here
+    console.log(foodTwo);
+} else {
+    console.log('your favourite food is unavailable');
+};
